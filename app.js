9137b9e7ad59fbdcbe939bb99850b1a82a36c6c1
AlexTicovschi <alexmticovschi@gmail.com>
2019-11-27 12:40:27 +0000

    connecting to MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+// app
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB Connected!'));
+
+// routes
 app.get('/', (req, res) => {
   res.send('Hello from NodeJS');
 });
