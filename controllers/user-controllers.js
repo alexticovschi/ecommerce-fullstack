1686ad0ef8c20d6cf10ce158f9b397c0ff9ed6ea
AlexTicovschi <alexmticovschi@gmail.com>
2019-11-27 17:31:36 +0000

    using errorHandler to return error friendly message

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,13 +1,16 @@
 const User = require('../models/user-model');
+const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.signup = (req, res) => {
   const user = new User(req.body);
-  user.save((err, user) => {
-    if (err) {
-      res.status(400).json({
-        error
+  user.save((error, user) => {
+    if (error) {
+      return res.status(400).json({
+        error: errorHandler(error)
       });
     }
+    user.salt = undefined;
+    user.hashed_password = undefined;
     res.json({
       user
     });
