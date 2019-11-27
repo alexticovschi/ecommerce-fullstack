0d13fe8f4d7083e4a4bcde48484c5f1b2e9179e6
AlexTicovschi <alexmticovschi@gmail.com>
2019-11-27 14:29:42 +0000

    adding virtual fields and methods to User Schema

--- models/user-model.js
+++ models/user-model.js
@@ -36,3 +36,31 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// virtual field
+userSchema
+  .virtual('password')
+  .set(function(password) {
+    this._password = password;
+    this.salt = uuid();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+userSchema.methods = {
+  encryptPassword: function(password) {
+    if (!password) return '';
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+module.exports = mongoose.model('User', userSchema);
