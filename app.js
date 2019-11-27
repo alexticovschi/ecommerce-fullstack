3e3e5573cf7edab33b13389bc11d9361ce32bee3
AlexTicovschi <alexmticovschi@gmail.com>
2019-11-27 12:25:22 +0000

    create a server using express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+  res.send('Hello from NodeJS');
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
