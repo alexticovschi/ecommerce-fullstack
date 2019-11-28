8109232a7ced9d4ddbab233bdf2315fe7f547564
AlexTicovschi <alexmticovschi@gmail.com>
2019-11-28 19:52:08 +0000

    using express validator

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 require('dotenv').config();
 
 // import routes
@@ -24,6 +25,7 @@ mongoose
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator());
 
 // route middlewares
 app.use('/api', userRoutes);
