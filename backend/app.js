3e32a30f9b5d3f893ba2657b67ddb1cc31cd463d
AlexTicovschi <alexmticovschi@gmail.com>
2019-12-04 14:54:02 +0000

    restructuring the app & adding the frontend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
@@ -29,6 +30,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 // route middlewares
 app.use('/api', authRoutes);
