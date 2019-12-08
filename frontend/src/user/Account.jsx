f2205ed3b9b270deb71fefc4808669351d4bdde9
AlexTicovschi <alexmticovschi@gmail.com>
2019-12-08 08:38:15 +0000

    adding links to user account

--- frontend/src/user/Account.jsx
+++ frontend/src/user/Account.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { isAuthenticated } from '../auth';
 
 import './account.scss';
@@ -8,31 +9,59 @@ const Account = () => {
     user: { _id, name, email, role }
   } = isAuthenticated();
 
+  const userLinks = () => {
+    return (
+      <ul className='account-links__list-group'>
+        <li className='account-links__list-group-item'>
+          <Link to='/cart'>My Cart</Link>
+        </li>
+        <li className='account-links__list-group-item'>
+          <Link to='/profile/update'>Update Profile</Link>
+        </li>
+      </ul>
+    );
+  };
+
   return (
     <section className='account-section'>
       <div className='container'>
-        <div className='account-content user-info'>
-          <h3 className='account-content__title'>User Information</h3>
+        <div className='account-wrapper'>
+          <div className='account-links'>
+            <h3 className='account-links__title'>User Links</h3>
+            <ul className='account-links__list-group'>
+              <li className='account-links__list-group-item'>
+                <Link to='/cart'>My Cart</Link>
+              </li>
+              <li className='account-links__list-group-item'>
+                <Link to='/profile/update'>Update Profile</Link>
+              </li>
+            </ul>
+          </div>
+          <div className='account-content'>
+            <div className='account-content__user-info'>
+              <h3 className='account-content__title'>User Information</h3>
 
-          <ul className='account-content__list-group'>
-            <li className='account-content__list-group-item'>
-              Name: <span>{name}</span>
-            </li>
-            <li className='account-content__list-group-item'>
-              Email: <span>{email}</span>
-            </li>
-            <li className='account-content__list-group-item'>
-              Role: <span>{role === 0 ? 'Registered User' : 'Admin'}</span>
-            </li>
-          </ul>
-        </div>
+              <ul className='account-content__list-group'>
+                <li className='account-content__list-group-item'>
+                  Name: <span>{name}</span>
+                </li>
+                <li className='account-content__list-group-item'>
+                  Email: <span>{email}</span>
+                </li>
+                <li className='account-content__list-group-item'>
+                  Role: <span>{role === 0 ? 'Registered User' : 'Admin'}</span>
+                </li>
+              </ul>
+            </div>
 
-        <div className='account-content purchase-history'>
-          <h3 className='account-content__title'>Purchase History</h3>
+            <div className='account-content__purchase-history'>
+              <h3 className='account-content__title'>Purchase History</h3>
 
-          <ul className='account-content__list-group'>
-            <li className='account-content__list-group-item'>History </li>
-          </ul>
+              <ul className='account-content__list-group'>
+                <li className='account-content__list-group-item'>History </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </section>
