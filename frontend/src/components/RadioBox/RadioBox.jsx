2836a2ee84bd26d1a0086f4479f02550b404d812
AlexTicovschi <alexmticovschi@gmail.com>
2019-12-14 14:55:06 +0000

    filtering by price range

--- frontend/src/components/RadioBox/RadioBox.jsx
+++ frontend/src/components/RadioBox/RadioBox.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
-import { prices } from './fixedPrices';
 import './radiobox.scss';
 
-const RadioBox = () => {
-  const [values, setValues] = useState([]);
+const RadioBox = ({ prices, handleFilters }) => {
+  const [value, setValue] = useState(0);
 
-  const handleChange = () => {};
+  const handleChange = event => {
+    handleFilters(event.target.value);
+    setValue(event.target.value);
+  };
 
   return prices.map(price => (
-    <div className='radiobox'>
+    <div className='radiobox' key={price._id}>
       <input
-        type='radio'
         className='radio-input'
+        type='radio'
         onChange={handleChange}
         value={price._id}
+        name={price}
       />
       <label className='price'>{price.name}</label>
     </div>
--- frontend/src/components/Shop/Shop.jsx
+++ frontend/src/components/Shop/Shop.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getAllCategories } from '../../api';
 import Checkbox from '../Checkbox/Checkbox';
 import RadioBox from '../RadioBox/RadioBox';
-
+import { prices } from '../../fixedPrices';
 import './shop.scss';
 
 const Shop = () => {
@@ -28,11 +28,36 @@ const Shop = () => {
     init();
   }, []);
 
+  // loop through prices and return the price range array ...
+  // that has an id equal to input value
+  const handlePrice = value => {
+    const pricesArray = prices;
+    let priceRange = [];
+
+    for (let key in pricesArray) {
+      if (pricesArray[key]._id === parseInt(value)) {
+        priceRange = pricesArray[key].array;
+      }
+    }
+
+    return priceRange;
+  };
+
   const handleFilters = (filters, filterBy) => {
     const newFilters = { ...myFilters };
     newFilters.filters[filterBy] = filters;
+
+    // 1# check if we filter by price
+    if (filterBy === 'price') {
+      // 2# store the return value of handlePrice function
+      const priceRange = handlePrice(filters);
+
+      // 3# add price range to newFilters array
+      newFilters.filters[filterBy] = priceRange;
+    }
+
+    // set state with new filter values
     setMyFilters(newFilters);
-    console.table(myFilters);
   };
 
   return (
@@ -54,7 +79,8 @@ const Shop = () => {
             <h3>Prices</h3>
             <div>
               <RadioBox
-                handleFilters={filters => handleFilters(filters, 'prices')}
+                prices={prices}
+                handleFilters={filters => handleFilters(filters, 'price')}
               />
             </div>
           </div>
