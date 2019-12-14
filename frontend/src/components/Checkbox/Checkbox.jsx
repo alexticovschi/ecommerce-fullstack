5ba516e7f9f4d4aca980a20394cd8a138c7441cc
AlexTicovschi <alexmticovschi@gmail.com>
2019-12-14 11:50:32 +0000

    handling multiple checkbox inputs

--- frontend/src/components/Checkbox/Checkbox.jsx
+++ frontend/src/components/Checkbox/Checkbox.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './checkbox.scss';
 
 const Checkbox = ({ categories }) => {
+  const [checked, setChecked] = useState([]);
+
+  const toggleCheckbox = catId => () => {
+    const currentCategoryId = checked.indexOf(catId);
+    const categoryIdsArray = [...checked];
+    if (currentCategoryId === -1) {
+      categoryIdsArray.push(catId);
+    } else {
+      categoryIdsArray.splice(currentCategoryId, 1);
+    }
+
+    setChecked(categoryIdsArray);
+  };
+
+  console.table(checked);
+
   return categories.map(category => (
     <li key={category._id}>
-      <input type='checkbox' className='checkbox-input' />
+      <input
+        type='checkbox'
+        value={checked.indexOf(category._id) === -1}
+        onChange={toggleCheckbox(category._id)}
+        className='checkbox-input'
+      />
       <label className='category-name'>{category.name}</label>
     </li>
   ));
